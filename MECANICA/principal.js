227f87904e7f4f28e2337a4b45309bda51981a4f
Erick Paredes Win <erikparedes246@gmail.com>
2024-01-30 20:07:10 -0500

    crear inicio
    creacion de pagina de inicio

---
+++ MECANICA/principal.js
@@ -0,0 +1,19 @@
+
+        const inicio = document.querySelector('.inicio');
+        const quiz = document.querySelector('.content_quiz');
+        const boton = document.querySelector('.boton_comenzar');
+        const volverInicio = document.querySelector('.volver_inicio');
+        
+        boton.addEventListener('click', () =>
+        {
+                inicio.style.display = 'none'; 
+                volverInicio.style.display = 'inline'; 
+                quiz.style.display = 'inline';  
+        });
+        
+        volverInicio.addEventListener('click', () =>
+        {
+                volverInicio.style.display = 'none';
+                inicio.style.display = 'inline'; 
+                quiz.style.display = 'none';  
+        });
